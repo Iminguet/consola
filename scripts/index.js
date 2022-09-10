92015c8837734a2029b816c53213579057579ec4
Iminguet <i.minguet@hotmail.com>
2022-09-11 00:49:14 +0200

    Optimización del boton power y creación de clases  para implementar el JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
 'use-strict';
+const botonPower = document.getElementById('botonEncendido');
+const luzPiloto = document.getElementById('pilotoDeEstado');
+
+console.log(botonPower, luzPiloto);
+
+botonPower.addEventListener('click', function () {
+  botonPower.classList.toggle('accionado');
+  luzPiloto.classList.toggle('luzEncendida');
+});
