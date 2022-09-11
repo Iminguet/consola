1335e17674c93aec9bb046ad55c81247d76f917c
Iminguet <i.minguet@hotmail.com>
2022-09-11 03:06:56 +0200

    Funcion que ambia la imagane con Javascript añadido. Imagenes añadidas.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,21 @@
 'use-strict';
 const botonPower = document.getElementById('botonEncendido');
 const luzPiloto = document.getElementById('pilotoDeEstado');
+let fondoPantalla = document.querySelector('.videoJuego');
 
-console.log(botonPower, luzPiloto);
+console.log(fondoPantalla);
+
+const numeroAleatorio = function () {
+  const numero = Math.trunc(Math.random() * 4) + 1;
+  fondoPantalla.src = `./img/pantalla-${numero}.gif`;
+};
 
 botonPower.addEventListener('click', function () {
   botonPower.classList.toggle('accionado');
   luzPiloto.classList.toggle('luzEncendida');
+  if (botonPower.classList.contains('accionado')) {
+    numeroAleatorio();
+  } else {
+    fondoPantalla.src = `./img/static.gif`;
+  }
 });
